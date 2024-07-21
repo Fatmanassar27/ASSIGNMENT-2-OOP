50531916df1ac015120e1133476255b983c471b7
Softlaptop <Softlaptop@DESKTOP-3CE5M2E>
2024-07-21 18:41:33 +0300

    vedio 1

---
+++ ASSIGNMENT 2 OOP/Encapsolation/Employee.cs
@@ -0,0 +1,71 @@
+﻿namespace ASSIGNMENT_2_OOP.Encapsolation
+{
+    internal struct Employee
+    {
+        public int Id;
+        private string? Name;
+        private decimal salary;
+        private int? age;
+
+        #region constractor
+        public Employee(int id, string? name, decimal salary, int age)
+
+        {
+            Id = id;
+            Name = name;
+            this.salary = salary;
+            this.age = age;
+        }
+
+        #endregion
+
+        #region method
+        public override string? ToString()
+        {
+            return $"Name = {Name} \nID = {Id} \nSalary = {salary}\nAge = {age}";
+        }
+
+        #endregion
+
+        #region setter and getter function 
+        public void Set_Name(string? name)
+        {
+            Name = name.Length > 5 ? name.Substring(0, 5) : name;
+        }
+        public string? Get_Name()
+        {
+            return Name;
+        }
+        #endregion
+
+        #region encapsulation using property
+
+        // full property 
+        public decimal Salary
+        {
+
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value < 3000 ? 3000 : value;
+            }
+
+        }
+
+        // automatic property
+
+        public int? Age { get; set; }
+
+        //Deduction
+        public decimal deduction
+        {
+            get { return (salary * 0.2M); }
+        }
+        #endregion
+
+
+    }
+}
--- ASSIGNMENT 2 OOP/Program.cs
+++ ASSIGNMENT 2 OOP/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace ASSIGNMENT_2_OOP
+﻿using ASSIGNMENT_2_OOP.Encapsolation;
+
+namespace ASSIGNMENT_2_OOP
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            Employee employee = new Employee(10, "fatma", 5000, 22);
+            Console.WriteLine(employee);
+            employee.Set_Name("rawda ali");
+            Console.WriteLine(employee.Get_Name());
+            employee.Salary = 1000;
+            Console.WriteLine(employee.Salary);
+            employee.Age = 23;
+            Console.WriteLine(employee.Age);
+            Console.WriteLine(employee.deduction);
         }
     }
 }
