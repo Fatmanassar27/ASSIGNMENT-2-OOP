31c5aaf163ff50734368dc8246b3d0b5443c27dd
Softlaptop <Softlaptop@DESKTOP-3CE5M2E>
2024-07-26 15:49:20 +0300

    v3

---
+++ ASSIGNMENT 2 OOP/Encapsolation/PhoneBook.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSIGNMENT_2_OOP.Encapsolation
+{
+    internal struct PhoneBook
+    {
+        int size;
+        string[] Name;
+        int[] PhoneNumbers;
+
+        //setter function of size 
+        public int Size
+        {
+            get { return size; }
+        }
+
+        // constructor
+        public PhoneBook(int size)
+        {
+            this.size = size;
+            Name = new string[size];
+            PhoneNumbers = new int[size];
+        }
+
+        // Add Person function
+        public void addperson(int position, string name, int number)
+        {
+            if (name != null && PhoneNumbers is not null)
+            {
+                if (position < size)
+                    PhoneNumbers[position] = number;
+                Name[position] = name;
+            }
+        }
+
+        // Encapsulation 
+        public int GetPhonenumber(string name)
+        {
+            if (PhoneNumbers is not null && Name is not null)
+            {
+                for (int i = 0; i < Name.Length; i++)
+                {
+                    if (Name[i] == name)
+                    {
+                        return PhoneNumbers[i];
+                    }
+                }
+            }
+            return -1;
+        }
+
+        //setter
+        public void add_newnumber(string name, int new_number)
+        {
+            if (name is not null && PhoneNumbers is not null)
+            {
+                for (int i = 0; i < Name.Length; i++)
+                {
+                    if (Name[i] == name)
+                    {
+                        PhoneNumbers[i] = new_number;
+                    }
+
+                }
+            }
+        }
+
+        //indexer 
+        public int this[string name]
+        {
+            get
+            {
+                if (Name is not null && PhoneNumbers is not null)
+                    for (int i = 0; i < Name.Length; i++)
+                        if (Name[i] == name)
+                        {
+                            return PhoneNumbers[i];
+                        }
+                return -1;
+            }
+
+            set
+            {
+                if (Name is not null && PhoneNumbers is not null)
+                    for (int i = 0; i < Name.Length; i++)
+                        if (Name[i] == name)
+                        {
+                            PhoneNumbers[i] = value;
+                        }
+            }
+        }
+
+        // indexer overloading 
+        public string this [int index]
+            {
+               get
+               {
+                return $"{index} : {Name[index]} : {PhoneNumbers[index]}";
+               }
+            }
+
+
+    }
+}
--- ASSIGNMENT 2 OOP/Program.cs
+++ ASSIGNMENT 2 OOP/Program.cs
@@ -6,6 +6,7 @@ namespace ASSIGNMENT_2_OOP
     {
         static void Main(string[] args)
         {
+            #region Encapsulation
             //Employee employee = new Employee(10, "fatma", 5000, 22);
             //Console.WriteLine(employee);
             //employee.Set_Name("rawda ali");
@@ -15,6 +16,32 @@ namespace ASSIGNMENT_2_OOP
             //employee.Age = 23;
             //Console.WriteLine(employee.Age);
             //Console.WriteLine(employee.deduction);
+            #endregion
+
+            #region property
+            //PhoneBook p1 = new PhoneBook(3);
+            //p1.addperson(0, "ahmed", 123);
+            //p1.addperson(1, "ali", 456);
+            //p1.addperson(2, "mohamed", 789);
+
+            //int person_number = p1.GetPhonenumber("alaa");
+            //Console.WriteLine(person_number == -1 ? "person is not found" : person_number );
+
+            //p1.add_newnumber("ali", 999);
+            //person_number = p1.GetPhonenumber("ali");
+            
+            //Console.WriteLine(person_number == -1 ? "person is not found" : person_number );
+
+            //Console.WriteLine(p1["ali"]);
+            //p1["ali"] = 111;
+            //Console.WriteLine(p1["ali"]);
+
+            //for (int i = 0; i < p1.Size; i++)
+            //{
+            //    Console.WriteLine(p1[i]);
+            //}
+            #endregion
+
         }
     }
 }
